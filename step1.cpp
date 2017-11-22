083ea7c7b520c158496f4d3c558a3075dba8f413
Adahai <chlsxbj@163.com>
2017-11-22 18:52:08 +0800

    第二步修改

---
+++ step1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "step1 进行成功！" << endl;
+	return 0;
+}
---
+++ step2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "这是第二步！" << endl;
+	return 0;
+}
